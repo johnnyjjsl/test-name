40b9ddfef291c72876a9e87ac6701104e5693431
Johnny Sanchez <johnnysanchez730@icloud.com>
2022-10-11 19:09:39 -0400

    Initial Commit

---
+++ test name/test_nameApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_nameApp.swift
+//  test name
+//
+//  Created by Johnny Sanchez on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct test_nameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
